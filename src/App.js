e8bfbc21dd79adfe5817b356233409dc085c82c8
prasanna-21 <prasannagurram00520@gmai.com>
2021-05-28 10:32:35 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+const api = {
+  key: "7b0440b1fd05cdbf124eff31d6555032",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [city, setCity] = useState({});
+
+  // console.log(query);
+
+  //call api for city when serach updated
+  useEffect(() => {
+    const searchCity = () => {
+      fetch(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
+        .then((res) => {
+          // console.log(res);
+          return res.json();
+        })
+        .then((result) => {
+          setCity(result);
+        });
+    };
+    searchCity();
+  }, [query]);
+
+  console.log(city);
+  let date = String(new window.Date());
+  // console.log(date);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="container">
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search city.."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+        {city.main ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {city.name},{city.sys.country}
+              </div>
+              <div className="time">{date.slice(0, 24)}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(city.main.temp)}c</div>
+
+              <div className="season">{city.weather[0].main}</div>
+            </div>
+          </div>
+        ) : (
+          <p style={{ textAlign: "center", color: "#fff" }}>City Not Found </p>
+        )}
+      </div>
     </div>
   );
 }
